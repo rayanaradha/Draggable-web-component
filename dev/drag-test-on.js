dd95f1208db3232ccca888ce518ea2de2662a0f5
rayanaradha <rayanaradha@gmail.com>
2021-01-27 14:55:43 +0530

    add input field and button

--- dev/drag-test-on.js
+++ dev/drag-test-on.js
@@ -4,8 +4,8 @@ class DragTestOnElement extends LitElement {
   static get styles() {
     return css`
         :host {
-            width: 110px;
-            height: 20px;
+            width: 180px;
+            height: 80px;
             display: block;
             margin: 10px 20px;
             position: relative;
@@ -14,6 +14,42 @@ class DragTestOnElement extends LitElement {
             border-radius: 4px;
             font-size: 15px;
             cursor: pointer;
+        }
+
+        .grid-container {
+          display: grid;
+          grid-template-columns: 70% 30%;
+          grid-gap: 5px;
+        
+        }  
+
+        input[type=text], select {
+            width: 100%;
+            padding: 10px 8px;
+            margin: 4px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+          width: 100%;
+          background-color: #4CAF50;
+          color: white;
+          padding: 10px 16px;
+          margin: 4px 0;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+          background-color: #45a049;
+        }
+
+        .draggable-part{
+        background-color:#F5F5F5;
         }
       
     `;
@@ -26,10 +62,19 @@ class DragTestOnElement extends LitElement {
         isAllowdToDrag: { type: Boolean }
       }  
   }  
+
+
   
   render() {
       return html`
-          <div class="box">${this.TestName} </div>
+          <div class="grid-container">
+              <div class="input-group">
+                  <input type="text" id="input" placeholder="${this.TestName} ">  
+                  <input type="submit" value="Submit">
+              </div>
+          <div class="draggable-part"></div>
+          </div>
+        
       `;
     }
 }
